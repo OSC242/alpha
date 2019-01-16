dd8f4965477177ecb0e7910425b4675b0b9caac5
Adnan RIHAN <adnan@rihan.fr>
2019-01-16 16:51:45 +0100

    Added /start | /help commands

--- routes/botman.php
+++ routes/botman.php
@@ -5,4 +5,16 @@ use App\Http\Controllers\BotManController;
 $botman = resolve('botman');
 $botname = config('botman.telegram.username');
 
-// Start Here
+$botman->hears("/(start|help)(?:@$botname)?", function ($bot) {
+    if ($bot->getUser()->getUsername()) {
+        $userStr = '@'.$bot->getUser()->getUsername();
+    } else {
+        $userStr = "[{$bot->getUser()->getFirstName()}](tg://user?id={$bot->getUser()->getId()})";
+    }
+
+    $msg = "$userStr, voici la liste des commandes:\n\n"
+          ."/start ou /help - Obtenir ce message\n"
+          ."/rule `sujet` - Obtenir la liste des règles ou une règle en particulier";
+
+    $bot->reply($msg, ['parse_mode' => 'markdown']);
+});
