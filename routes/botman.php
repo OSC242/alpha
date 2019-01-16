5685dd1c9d05d2b0c7282e75e9f069a2e229fa23
Adnan RIHAN <adnan@rihan.fr>
2019-01-16 20:14:13 +0100

    Added basic /rule command
    - audio|voix
    - politique

--- routes/botman.php
+++ routes/botman.php
@@ -18,3 +18,24 @@ $botman->hears("/(start|help)(?:@$botname)?", function ($bot) {
 
     $bot->reply($msg, ['parse_mode' => 'markdown']);
 });
+
+$botman->hears("/rule(?:@$botname)?(?: (.*))?", function ($bot, $subject = null) {
+    if ($bot->getUser()->getUsername()) {
+        $userStr = '@'.$bot->getUser()->getUsername();
+    } else {
+        $userStr = "[{$bot->getUser()->getFirstName()}](tg://user?id={$bot->getUser()->getId()})";
+    }
+
+    if (is_null($subject)) {
+        $msg = "$userStr, quel sujet recherches-tu?\n\n"
+              ."- `audio`\n"
+              ."- `politique`\n\n"
+              ."Il te suffit de taper: /rule `sujet` pour avoir les détails";
+    } elseif (in_array($subject, ['audio', 'voix'])) {
+        $msg = 'Les *messages vocaux* ne sont pas autorisés dans ce groupe. Si vous tentez d\'en envoyer un, il sera supprimé. Plusieurs tentatives pourront donner lieu à une éjection du groupe.';
+    } elseif ($subject == 'politique') {
+        $msg = 'Nous ne faisons pas de *politique*, ces sujets sont interdits. Plusieurs tentatives pourront donner lieu à une éjection du groupe.';
+    }
+
+    $bot->reply($msg, ['parse_mode' => 'markdown']);
+});
